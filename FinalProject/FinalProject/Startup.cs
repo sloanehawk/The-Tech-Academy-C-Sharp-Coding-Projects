5c485c4d0a88adf8a55c586c5f00544e36ab6cae
Sloane <sloanehawkins@gmail.com>
2020-02-21 18:21:40 -0800

    Add final project
    Final project uses knockout.js

---
+++ FinalProject/FinalProject/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(FinalProject.Startup))]
+
+namespace FinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
